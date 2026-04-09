5be89aa0299c7da80575d7988398ddbc55f857f5
Joar Wathne <wathne@users.noreply.github.com>
2026-04-09 15:56:36 +0200

    feat: add template for private API keys
    This is a template because my own private API keys have been redacted.
    
    Set your environment variables or populate "./private_api_keys.py" with
    your own private API keys if you want to use LLM functionality (GPT,
    Llama, Claude, and Gemini). YouTube transcripts (closed captions) are
    fetched without an API key, but other video metadata requires a private
    API key. Modules will try to read a wanted API key from your environment
    variables if not found in "./private_api_keys.py".
    
    Complete list of API key environment variables:
    ANTHROPIC_CLAUDE_API_KEY
    GOOGLE_GEMINI_API_KEY
    GOOGLE_YOUTUBE_API_KEY
    META_LLAMA_API_KEY
    OPENAI_GPT_API_KEY
    
    new file:   private_api_keys.py

---
+++ private_api_keys.py
@@ -0,0 +1,37 @@
+# Private API keys.
+
+
+# Anthropic API key for Claude LLM.
+# https://platform.claude.com/docs/en/get-started
+# https://platform.claude.com/settings/keys
+ANTHROPIC_CLAUDE_API_KEY: str = (
+    "" # REDACTED
+)
+
+# Google API key for Gemini LLM.
+# https://ai.google.dev/gemini-api/docs/quickstart
+# https://aistudio.google.com/app/apikey
+GOOGLE_GEMINI_API_KEY: str = (
+    "" # REDACTED
+)
+
+# Google API key for YouTube video metadata.
+# https://developers.google.com/youtube/v3/getting-started
+# https://console.cloud.google.com/apis/credentials
+GOOGLE_YOUTUBE_API_KEY: str = (
+    "" # REDACTED
+)
+
+# Meta API key for Llama LLM.
+# https://llama.developer.meta.com/docs/quickstart
+# https://llama.developer.meta.com/
+META_LLAMA_API_KEY: str = (
+    ""
+)
+
+# OpenAI API key for GPT LLM.
+# https://developers.openai.com/api/docs/quickstart
+# https://platform.openai.com/api-keys
+OPENAI_GPT_API_KEY: str = (
+    "" # REDACTED
+)
